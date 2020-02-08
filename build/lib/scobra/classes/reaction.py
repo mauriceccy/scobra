b0de50fde21d7fbc6b8f9f2c571a060d8a593ebb
ajsanjoaquin <ajsanjoaquin@gmail.com>
2020-02-08 11:43:07 +0800

    Update package

---
+++ build/lib/scobra/classes/reaction.py
@@ -0,0 +1,28 @@
+import builtins as exceptions
+#many unsupported attribute of types lib in python 3
+import types
+import re, math 
+from collections import defaultdict
+import numpy
+import scipy
+#import metabolite
+
+import cobra
+from cobra import Metabolite, Reaction, Gene
+from cobra.flux_analysis import deletion, moma, phenotype_phase_plane
+from cobra.core.solution import get_solution
+#from cobra.manipulation import modify
+
+#from ..analysis import FCA, Pareto, RWFM, MOMA, ROOM, GeometricFBA, MinSolve
+#from ..analysis import Graph, FluxSum, FVA, MinSolve, Scan
+#from ..manipulation import Reversible
+#from ..classes.flux import flux
+#rom ..io import Network
+
+class Reaction(cobra.Reaction):
+	def __init__(self,id=None, name='', subsystem='', lower_bound=0.,upper_bound=1000., objective_coefficient=0.):
+		super().__init__(id=id,name=name,subsystem=subsystem,lower_bound=lower_bound,upper_bound=upper_bound,objective_coefficient=objective_coefficient)
+		
+		return
+	def __str__(self):
+		return
---
+++ build/lib/scobra/io/metabolite.py
@@ -0,0 +1,41 @@
+import builtins as exceptions
+#many unsupported attribute of types lib in python 3
+import types
+import re, math 
+from collections import defaultdict
+import numpy
+import scipy
+
+import cobra
+from cobra import Metabolite, Reaction, Gene
+from cobra.flux_analysis import deletion, moma, phenotype_phase_plane
+from cobra.core.solution import get_solution
+#from cobra.manipulation import modify
+
+#from ..analysis import FCA, Pareto, RWFM, MOMA, ROOM, GeometricFBA, MinSolve
+#from ..analysis import Graph, FluxSum, FVA, MinSolve, Scan
+#from ..manipulation import Reversible
+#from ..classes.flux import flux
+#from ..io import Network
+
+class Metabolite(cobra.Metabolite):
+	"""Metabolite is a class inherited from the cobra Metabolite class that holds information
+	that can be used in the scobra reaction class.  It differs forom the original cobra
+	Metabolite class with 3 new states:
+
+	Parameters
+	----------
+	inchi :
+	smiles :
+	molecular_weights : float
+		the combined weight of the molecules in the metabolite
+	"""
+
+	def __init__(self,id=None, formula=None, name="",charge=None, compartment=None,inchi=None,smiles=None,molecular_weights=None):
+		super().__init__(id=id, formula=formula, name=name,charge=charge, compartment=compartment)
+		self.inchi = inchi
+		self.smiles = smiles
+		self.molecular_weights = molecular_weights
+
+	def __str__(self):
+		return
