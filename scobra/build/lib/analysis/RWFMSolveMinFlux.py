d6f6d9aa87ec07ce75c767d8fe6dcec8478e6429
sewenthy <sewenthy@sewen.local>
2020-05-11 14:15:59 +0800

    removed build inside

--- scobra/build/lib/analysis/RWFMSolveMinFlux.py
+++
@@ -1,5 +0,0 @@
-def RWFMSolveMinFlux(model):
-    model.ZeroObjective()
-    model.MinFluxSolve(PrintStatus=False,weighting='random')
-    sol = model.GetSol(IncZeroes=True)
-    return sol
--- scobra/build/lib/analysis/test.py
+++
@@ -1,5 +0,0 @@
-def sumsquare(number):
-    s=0
-    for i in range (number):
-        s+=i * i
-    return s
