caef57b4819a9f9270c158a718731f3f3e1e79e5
sewenthy <sewenthy@sewen.local>
2020-05-12 10:10:23 +0800

    Update 1.2.8: added generation file for requirements

---
+++ get_reqs.py
@@ -0,0 +1,25 @@
+import os
+
+folders = ["analysis", "classes", "io", "manipulation"]
+reqs = {}
+
+for v in folders:
+    os.system(os.path.join("pipreqs scobra",v,""))
+    with open(os.path.join("scobra",v,"requirements.txt"),'r') as req_file:
+        line = req_file.readline()
+        while(line != ""):
+            name, version = line.split("==")
+            version = version.rstrip("\n")
+            if name in reqs:
+                if reqs[name] < version:
+                    line = req_file.readline()
+                    continue
+            reqs[name] = version
+            line = req_file.readline()
+
+    os.system(os.path.join("rm scobra",v,"requirements.txt"))
+
+with open("requirements.txt", "w") as req_file:
+    for n in reqs:
+        req_file.write(n+">="+reqs[n]+"\n")
+        
