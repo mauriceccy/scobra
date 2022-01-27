9a8dec50e7d86ccb702a9450c537699f677fc5c2
nihalzp <81457724+nihalzp@users.noreply.github.com>
2022-01-27 14:42:41 +0800

    Make Excel.py equilibrium const compatible

--- scobra/io/Excel.py
+++ scobra/io/Excel.py
@@ -26,6 +26,7 @@ def ReadExcel(excel_file, parse="cobra_string", Print=False):
     %   'EC Number'         2.7.1.1,2.7.1.2
     %   'Notes'             'Reaction also associated with EC 2.7.1.2' (optional)
     %   'References'        PMID:2043117,PMID:7150652,...   (optional)
+    %   'Equilibrium Constant' Any number (optional) 
     %   'Rate Constant'     0   (optional)
     %   'Rate Equation'     A ** 2 * B ** 1     (optional)
     %   'Exchange Reaction' None or metabolite.id
@@ -101,12 +102,13 @@ def ReadExcel(excel_file, parse="cobra_string", Print=False):
         elif 'metabolite' in sheet.lower():
             metabolites = excel.parse(sheet,index_col=None)
     # Caution will the following line work if no last 3 columns are present??
-    cobra_reaction_position = ['Abbreviation','Description','Reaction','GPR','Genes','Proteins','Subsystem','Reversible','Lower bound','Upper bound','Objective','Confidence Score','EC Number','Notes','References','Rate Constant', 'Rate Equation', 'Exchange Reaction']
+    cobra_reaction_position = ['Abbreviation','Description','Reaction','GPR','Genes','Proteins','Subsystem','Reversible','Lower bound','Upper bound','Objective','Confidence Score','EC Number',
+                               'Equilibrium Constant','Notes','References','Rate Constant','Rate Equation','Exchange Reaction']
     cobra_metabolite_position = ['Abbreviation','Description','Neutral formula','Charged formula','Charge','Compartment','KEGG ID','PubChem ID','ChEBI ID','InChI string','SMILES','HMDB ID']
 
     if parse == "cobra_position":
-        if len(reactions.columns) > 17:
-            reactions = reactions.iloc[:,:17]
+        if len(reactions.columns) > 18:
+            reactions = reactions.iloc[:,:18]
             reactions.columns = cobra_reaction_position
         else:
             reactions.columns = cobra_reaction_position[:len(reactions.columns)]
@@ -258,6 +260,8 @@ def ReadExcel(excel_file, parse="cobra_string", Print=False):
         if ('EC Number' in reac_row.index) and pandas.notnull(reac_row['EC Number']): reaction.ec_number = str(reac_row['EC Number'])
         if ('Notes' in reac_row.index) and pandas.notnull(reac_row['Notes']): reaction.notes = {"notes":str(reac_row['Notes'])}
         if ('References' in reac_row.index) and pandas.notnull(reac_row['References']): reactions.references = str(reac_row['References'])
+        if ('Equilibrium Constant' in reac_row.index) and pandas.notnull(reac_row['Equilibrium Constant']):
+            reaction.equilibrium_constant = int(reac_row['Equilibrium Constant'])
         if ('Rate Constant' in reac_row.index) and pandas.notnull(reac_row['Rate Constant']):
             reaction.rate_constant = int(reac_row['Rate Constant'])
         if ('Rate Equation' in reac_row.index) and pandas.notnull(reac_row['Rate Equation']):
